5e3fd702b3708fa16a475ee33a6562ade885c9be
DaiF <julie.fiadino@gmail.com>
2025-01-01 23:28:51 +0100

    update: color change

--- confs/config/nvim/lua/plugins.lua
+++ confs/config/nvim/lua/plugins.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     'ayu-theme/ayu-vim',
+    'ribru17/bamboo.nvim',
     'nvim-lualine/lualine.nvim',
     {
         'neoclide/coc.nvim',
